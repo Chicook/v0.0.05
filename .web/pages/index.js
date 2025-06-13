55419312ea3173ad9c0d40ef596885f184f3a885
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2025-06-13 22:39:05 +0200

    refactor: reorganize imports and update event handling in dropdown components

--- .web/pages/index.js
+++ .web/pages/index.js
@@ -10,6 +10,90 @@ import { jsx } from "@emotion/react"
 
 
 
+export function Dropdownmenu__item_de6b302b3f1c19ec452a77ae1b027ec6 () {
+  
+  const [addEvents, connectErrors] = useContext(EventLoopContext);
+
+
+  const on_click_dbc56e49772170ff2adde4c5e0901692 = useCallback(((...args) => (addEvents([(Event("reflex___state____state.metaverso_crypto_3_d___metaverso_crypto_3_d____state.select_network", ({ ["network"] : "Ethereum" }), ({  })))], args, ({  })))), [addEvents, Event])
+
+
+
+  
+  return (
+    jsx(
+RadixThemesDropdownMenu.Item,
+{onClick:on_click_dbc56e49772170ff2adde4c5e0901692},
+"Ethereum"
+,)
+  )
+}
+
+export function Dropdownmenu__item_dae734d98dc77c867b3b4599faea032d () {
+  
+  const [addEvents, connectErrors] = useContext(EventLoopContext);
+
+
+  const on_click_ecad624a83ecc5177bccafbea2f26ea0 = useCallback(((...args) => (addEvents([(Event("reflex___state____state.metaverso_crypto_3_d___metaverso_crypto_3_d____state.select_network", ({ ["network"] : "Binance Smart Chain" }), ({  })))], args, ({  })))), [addEvents, Event])
+
+
+
+  
+  return (
+    jsx(
+RadixThemesDropdownMenu.Item,
+{onClick:on_click_ecad624a83ecc5177bccafbea2f26ea0},
+"Binance Smart Chain"
+,)
+  )
+}
+
+export function Dropdownmenu__item_37ba5000536699318e7d5893b95df8da () {
+  
+  const [addEvents, connectErrors] = useContext(EventLoopContext);
+
+
+  const on_click_447bb4e82cb1926a67d0ed70436472be = useCallback(((...args) => (addEvents([(Event("reflex___state____state.metaverso_crypto_3_d___metaverso_crypto_3_d____state.select_network", ({ ["network"] : "Solana" }), ({  })))], args, ({  })))), [addEvents, Event])
+
+
+
+  
+  return (
+    jsx(
+RadixThemesDropdownMenu.Item,
+{onClick:on_click_447bb4e82cb1926a67d0ed70436472be},
+"Solana"
+,)
+  )
+}
+
+export function Dropdownmenu__root_f52dc4acc3a89778e0b960c8c7011647 () {
+  
+  const reflex___state____state__metaverso_crypto_3_d___metaverso_crypto_3_d____state = useContext(StateContexts.reflex___state____state__metaverso_crypto_3_d___metaverso_crypto_3_d____state)
+  const [addEvents, connectErrors] = useContext(EventLoopContext);
+
+
+  const on_open_change_de957873a4c04b7beb60a4100a3782d3 = useCallback(((_ev_0) => (addEvents([(Event("reflex___state____state.metaverso_crypto_3_d___metaverso_crypto_3_d____state.set_show_networks_menu", ({ ["value"] : _ev_0 }), ({  })))], [_ev_0], ({  })))), [addEvents, Event])
+
+
+
+  
+  return (
+    jsx(
+RadixThemesDropdownMenu.Root,
+{css:({ ["isOpen"] : reflex___state____state__metaverso_crypto_3_d___metaverso_crypto_3_d____state.show_networks_menu }),onOpenChange:on_open_change_de957873a4c04b7beb60a4100a3782d3},
+jsx(Dropdownmenu__trigger_e8c7a03e03361f54a8d00743b8c293c5,{},)
+,jsx(
+RadixThemesDropdownMenu.Content,
+{},
+jsx(Dropdownmenu__item_de6b302b3f1c19ec452a77ae1b027ec6,{},)
+,jsx(Dropdownmenu__item_c62a309dd480b72b95a1f7863802867e,{},)
+,jsx(Dropdownmenu__item_dae734d98dc77c867b3b4599faea032d,{},)
+,jsx(Dropdownmenu__item_37ba5000536699318e7d5893b95df8da,{},)
+,),)
+  )
+}
+
 export function Dropdownmenu__item_c62a309dd480b72b95a1f7863802867e () {
   
   const [addEvents, connectErrors] = useContext(EventLoopContext);
@@ -47,90 +131,6 @@ RadixThemesButton,
 {css:({ ["background"] : ((reflex___state____state__metaverso_crypto_3_d___metaverso_crypto_3_d____state.selected_network === "Redes Blockchain") ? "#6c757d" : "#28a745"), ["color"] : "white", ["borderRadius"] : "md", ["&:hover"] : ({ ["background"] : ((reflex___state____state__metaverso_crypto_3_d___metaverso_crypto_3_d____state.selected_network === "Redes Blockchain") ? "#5a6268" : "#218838") }) }),size:({ ["initial"] : "1", ["md"] : "2" })},
 reflex___state____state__metaverso_crypto_3_d___metaverso_crypto_3_d____state.selected_network
 ,),)
-  )
-}
-
-export function Dropdownmenu__item_de6b302b3f1c19ec452a77ae1b027ec6 () {
-  
-  const [addEvents, connectErrors] = useContext(EventLoopContext);
-
-
-  const on_click_dbc56e49772170ff2adde4c5e0901692 = useCallback(((...args) => (addEvents([(Event("reflex___state____state.metaverso_crypto_3_d___metaverso_crypto_3_d____state.select_network", ({ ["network"] : "Ethereum" }), ({  })))], args, ({  })))), [addEvents, Event])
-
-
-
-  
-  return (
-    jsx(
-RadixThemesDropdownMenu.Item,
-{onClick:on_click_dbc56e49772170ff2adde4c5e0901692},
-"Ethereum"
-,)
-  )
-}
-
-export function Dropdownmenu__root_f52dc4acc3a89778e0b960c8c7011647 () {
-  
-  const reflex___state____state__metaverso_crypto_3_d___metaverso_crypto_3_d____state = useContext(StateContexts.reflex___state____state__metaverso_crypto_3_d___metaverso_crypto_3_d____state)
-  const [addEvents, connectErrors] = useContext(EventLoopContext);
-
-
-  const on_open_change_de957873a4c04b7beb60a4100a3782d3 = useCallback(((_ev_0) => (addEvents([(Event("reflex___state____state.metaverso_crypto_3_d___metaverso_crypto_3_d____state.set_show_networks_menu", ({ ["value"] : _ev_0 }), ({  })))], [_ev_0], ({  })))), [addEvents, Event])
-
-
-
-  
-  return (
-    jsx(
-RadixThemesDropdownMenu.Root,
-{css:({ ["isOpen"] : reflex___state____state__metaverso_crypto_3_d___metaverso_crypto_3_d____state.show_networks_menu }),onOpenChange:on_open_change_de957873a4c04b7beb60a4100a3782d3},
-jsx(Dropdownmenu__trigger_e8c7a03e03361f54a8d00743b8c293c5,{},)
-,jsx(
-RadixThemesDropdownMenu.Content,
-{},
-jsx(Dropdownmenu__item_de6b302b3f1c19ec452a77ae1b027ec6,{},)
-,jsx(Dropdownmenu__item_c62a309dd480b72b95a1f7863802867e,{},)
-,jsx(Dropdownmenu__item_dae734d98dc77c867b3b4599faea032d,{},)
-,jsx(Dropdownmenu__item_37ba5000536699318e7d5893b95df8da,{},)
-,),)
-  )
-}
-
-export function Dropdownmenu__item_37ba5000536699318e7d5893b95df8da () {
-  
-  const [addEvents, connectErrors] = useContext(EventLoopContext);
-
-
-  const on_click_447bb4e82cb1926a67d0ed70436472be = useCallback(((...args) => (addEvents([(Event("reflex___state____state.metaverso_crypto_3_d___metaverso_crypto_3_d____state.select_network", ({ ["network"] : "Solana" }), ({  })))], args, ({  })))), [addEvents, Event])
-
-
-
-  
-  return (
-    jsx(
-RadixThemesDropdownMenu.Item,
-{onClick:on_click_447bb4e82cb1926a67d0ed70436472be},
-"Solana"
-,)
-  )
-}
-
-export function Dropdownmenu__item_dae734d98dc77c867b3b4599faea032d () {
-  
-  const [addEvents, connectErrors] = useContext(EventLoopContext);
-
-
-  const on_click_ecad624a83ecc5177bccafbea2f26ea0 = useCallback(((...args) => (addEvents([(Event("reflex___state____state.metaverso_crypto_3_d___metaverso_crypto_3_d____state.select_network", ({ ["network"] : "Binance Smart Chain" }), ({  })))], args, ({  })))), [addEvents, Event])
-
-
-
-  
-  return (
-    jsx(
-RadixThemesDropdownMenu.Item,
-{onClick:on_click_ecad624a83ecc5177bccafbea2f26ea0},
-"Binance Smart Chain"
-,)
   )
 }
 
