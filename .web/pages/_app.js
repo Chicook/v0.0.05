642e9034e5609224b1cf6a9155846f1d0c76a72d
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2025-06-15 22:25:25 +0200

    feat: Add deployment scripts for Storage contract using ethers.js and web3.js
    - Created deploy_with_ethers.ts for deploying the Storage contract with ethers.js.
    - Created deploy_with_web3.ts for deploying the Storage contract with web3.js.
    - Added ethers-lib.ts to handle contract deployment using ethers.js.
    - Added web3-lib.ts to handle contract deployment using web3.js.
    
    feat: Implement WoldcoinVirtual contract and related artifacts
    
    - Added WoldcoinVirtual contract with basic ERC20 functionality.
    - Included contract metadata and ABI in JSON format for deployment.
    - Created tests for the Ballot contract to ensure functionality.
    - Added storage tests for the Storage contract to verify initial and updated values.
    
    chore: Add frontend files for decentralized login
    
    - Added index.html for the decentralized login interface.
    - Included favicon.ico and favicon.png for branding.
    - Created manifest.json for PWA support.

--- .web/pages/_app.js
+++ .web/pages/_app.js
@@ -16,12 +16,12 @@ import { DefaultOverlayComponents, MemoizedToastProvider } from "$/utils/compone
 import { EventLoopProvider, StateProvider, defaultColorMode } from "$/utils/context.js";
 import { ThemeProvider } from 'next-themes'
 import * as React from "react";
-import * as utils_context from "$/utils/context";
-import * as next_link from "next/link";
-import * as emotion_react from "@emotion/react";
-import * as radix_ui_themes from "@radix-ui/themes";
-import * as utils_components from "$/utils/components";
 import * as utils_state from "$/utils/state";
+import * as emotion_react from "@emotion/react";
+import * as utils_components from "$/utils/components";
+import * as next_link from "next/link";
+import * as radix_ui_themes from "@radix-ui/themes";
+import * as utils_context from "$/utils/context";
 
 
 function AppWrap({children}) {
@@ -65,12 +65,12 @@ export default function MyApp({ Component, pageProps }) {
     // Make contexts and state objects available globally for dynamic eval'd components
     let windowImports = {
       "react": React,
-      "$/utils/context": utils_context,
-      "next/link": next_link,
-      "@emotion/react": emotion_react,
-      "@radix-ui/themes": radix_ui_themes,
-      "$/utils/components": utils_components,
       "$/utils/state": utils_state,
+      "@emotion/react": emotion_react,
+      "$/utils/components": utils_components,
+      "next/link": next_link,
+      "@radix-ui/themes": radix_ui_themes,
+      "$/utils/context": utils_context,
     };
     window["__reflex"] = windowImports;
   }, []);
