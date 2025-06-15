311c77f9e1c86a226ddcd002a222e8d7072684fd
Juan Angel Lopez Diaz <102910494+Chicook@users.noreply.github.com>
2025-06-15 22:12:36 +0200

    add

--- rxconfig.py
+++
@@ -1,8 +0,0 @@
-import reflex as rx
-
-config = rx.Config(
-    app_name="WoldVirtual_Crypto_3D",
-    db_url="sqlite:///reflex.db",
-    env=rx.Env.DEV,
-    tailwind=None,  # Desactivamos Tailwind ya que no lo estamos usando
-) 
